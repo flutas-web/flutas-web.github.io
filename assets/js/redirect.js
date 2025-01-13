e75f90e9a6c3ba5d7a9b431e4ebd9b7e74437493
flutas-web <flutas@outlook.com>
2025-01-13 08:29:48 +0800

    a

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -5,24 +5,25 @@
             let t = win.location.hash.split('#/')[1];
             switch (true) {
                 case t.startsWith('a'):
-                    return { type: 'article', 'id': t.substring(1) }
+                    return { type: 'article', 'id': t.substring(1).split('?')[0] }
                 default:
-                    return { type: 'undefined', 'id': '000' };
+                    return { type: 'undefined', 'id': '00' };
             }
         },
-        c: (f,e) => { con.log(f,e == undefined ? '' : e) },
+        c: (f, e) => { con.log(f, e == undefined ? '' : e) },
         crm: (content) => {
             try {
-                if(aluk(`flutas-main`).length === 0 || aluk(`flutas-main`).length === undefined) {
+                if (aluk(`flutas-main`).length === 0 || aluk(`flutas-main`).length === undefined) {
                     gt.flutas_main = document.createElement('flutas-main');
                     doc.body.appendChild(gt.flutas_main);
                 }
                 gt.flutas_main.shadow = gt.flutas_main.getShadowRoot();
                 gt.flutas_main.shadow.querySelector('.flutas-main.inner').innerHTML = marked.parse(content.content);
-            } catch(t) {
-                funcs.c('error loading page',t);
+                doc.title = content.title + ` - Flutas`;
+            } catch (t) {
+                funcs.c('error loading page', t);
             }
-            
+
         },
         loadf: (json) => {
             funcs.crm(json);
@@ -32,12 +33,12 @@
     let redirect = {
         article: {
             init: (id) => {
-                fetch(`../registry/saves/${id}.json`).then((res) => res.json()).then(f=>{funcs.loadf(f)}).catch(e => {funcs.loade(e)})
-            }   
+                fetch(`../registry/saves/${id}.json`).then((res) => res.json()).then(f => { funcs.loadf(f) }).catch(e => { funcs.loade(e) })
+            }
         },
         notice: {
             init: (id) => {
-                fetch(`../registry/notices/${id}.json`).then((res) => res.json()).then(f=>{funcs.loadf(f)}).catch(e => {funcs.loade(e)})
+                fetch(`../registry/notices/${id}.json`).then((res) => res.json()).then(f => { funcs.loadf(f) }).catch(e => { funcs.loade(e) })
             }
         }
     };
@@ -53,12 +54,42 @@
                 redirect.notice.init(t.id);
                 break;
             default:
-                funcs.c('undefined');
+                redirect.article.init('00');
                 break;
         }
     } else {
-        funcs.c('undefined');
+        redirect.article.init('00');
     }
+
+
+
+
+    function scrollToElement(elementId) {
+        const element = document.getElementById(elementId);
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' }); // 平滑滚动
+        } else {
+            console.log('元素未找到:', elementId);
+        }
+    }
+
+    function scrollToEle() {
+        let params = new URLSearchParams(win.location.hash.split('?')[1]);
+        let posValue = params.get('pos');
+        if(posValue != null) {
+            scrollToElement(posValue);
+        }
+    }
+    // 定义要执行的函数
+    function onUrlChange() {
+        if (!a()) {
+            redirect.article.init('00');
+        }
+        scrollToEle();
+    }
+    win.addEventListener('hashchange', onUrlChange);
+    win.addEventListener('popstate', onUrlChange);
+
 }
 )({
     gt: globalThis,
